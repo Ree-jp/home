32878d8f15b62e90b31d6655290153e731c8f92a
ree-jp <mail@ree-jp.net>
2026-05-19 15:11:40 +0900

    ⚡️ bgサイズを縮小

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,9 +6,8 @@
 </script>
 
 <div class="bg-[#fdfbf8] min-h-screen relative overflow-x-hidden">
-  <!-- Figmaデザインのトップ装飾画像。コンテナh=65px、画像h=1440px、top=-1023pxでスライスを表示 -->
   <div class="absolute top-[-32px] left-0 w-full pointer-events-none overflow-hidden" style="height: 65px;">
-    <img alt="" src="/images/bg-reef.png" class="absolute w-full" style="height: 1440px; top: -1023px;" />
+    <img alt="" src="/images/bg-reef-crop.webp" class="absolute w-full h-full" style="object-fit: cover; object-position: center 43%;" />
   </div>
 
   {@render children()}
